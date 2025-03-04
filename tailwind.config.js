932ddb0fcf40193a195c3d0a5bacebfd4183284f
Ahmed <ahmedabozaid.dev@gmail.com>
2025-03-04 11:14:36 +0200

    changes font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ["Roboto"],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
