6ed5a902902b5b5c3c50d672810de11a775640be
Ahmed <ahmedabozaid.dev@gmail.com>
2025-03-06 10:08:45 +0200

    adds animation to testimonials

--- src/components/HomePage/Testimonials.jsx
+++ src/components/HomePage/Testimonials.jsx
@@ -1,9 +1,15 @@
 import img from "@/assets/images/testimonials.webp";
+import { useInView } from "react-intersection-observer";
 import Card from "../constants/Home/Card";
+import AnimateOnScroll from "../general/AnimateOnScroll";
 
 const Testimonials = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: "-300px 0px",
+  });
   return (
-    <section className="container">
+    <section ref={ref} className="container">
       <h1 className="text-[#121212] md:text-[32px] text-lg flex justify-center md:leading-8">
         Testimonials
       </h1>
@@ -11,33 +17,41 @@ const Testimonials = () => {
         <span className="text-brandColor">Hear&nbsp;</span> From Us{" "}
       </h2>
       <div className="mt-12 grid lg:grid-cols-2 grid-cols-1 gap-4">
-        <Card
-          text1="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever..."
-          text2="At AQuadic, we specialize in delivering innovative software and design-driven solutions that help startups and businesses thrive. Our team works closely with clients to bring their ideas to life..."
-          imgSrc={img}
-          rating="8.9"
-        />
+        <AnimateOnScroll inView={inView} delay={0.2}>
+          <Card
+            text1="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever..."
+            text2="At AQuadic, we specialize in delivering innovative software and design-driven solutions that help startups and businesses thrive. Our team works closely with clients to bring their ideas to life..."
+            imgSrc={img}
+            rating="8.9"
+          />
+        </AnimateOnScroll>
 
-        <Card
-          text1="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever..."
-          text2="At AQuadic, we specialize in delivering innovative software and design-driven solutions that help startups and businesses thrive. Our team works closely with clients to bring their ideas to life..."
-          imgSrc={img}
-          rating="8.9"
-        />
+        <AnimateOnScroll inView={inView} delay={0.4}>
+          <Card
+            text1="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever..."
+            text2="At AQuadic, we specialize in delivering innovative software and design-driven solutions that help startups and businesses thrive. Our team works closely with clients to bring their ideas to life..."
+            imgSrc={img}
+            rating="8.9"
+          />
+        </AnimateOnScroll>
 
-        <Card
-          text1="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever..."
-          text2="At AQuadic, we specialize in delivering innovative software and design-driven solutions that help startups and businesses thrive. Our team works closely with clients to bring their ideas to life..."
-          imgSrc={img}
-          rating="8.9"
-        />
+        <AnimateOnScroll inView={inView} delay={0.6}>
+          <Card
+            text1="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever..."
+            text2="At AQuadic, we specialize in delivering innovative software and design-driven solutions that help startups and businesses thrive. Our team works closely with clients to bring their ideas to life..."
+            imgSrc={img}
+            rating="8.9"
+          />
+        </AnimateOnScroll>
 
-        <Card
-          text1="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever..."
-          text2="At AQuadic, we specialize in delivering innovative software and design-driven solutions that help startups and businesses thrive. Our team works closely with clients to bring their ideas to life..."
-          imgSrc={img}
-          rating="8.9"
-        />
+        <AnimateOnScroll inView={inView} delay={0.8}>
+          <Card
+            text1="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever..."
+            text2="At AQuadic, we specialize in delivering innovative software and design-driven solutions that help startups and businesses thrive. Our team works closely with clients to bring their ideas to life..."
+            imgSrc={img}
+            rating="8.9"
+          />
+        </AnimateOnScroll>
       </div>
     </section>
   );
