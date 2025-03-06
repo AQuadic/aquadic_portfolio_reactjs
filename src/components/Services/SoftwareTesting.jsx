58d082a96e14248c4486afddc444b40a09c89405
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-03-06 11:01:48 +0200

    Add Testing services in constants folder

--- src/components/Services/SoftwareTesting.jsx
+++ src/components/Services/SoftwareTesting.jsx
@@ -1,12 +1,6 @@
-import Services from "../general/Services"
-import mobApp from '../../assets/images/mobApp.svg'
-import quality from '../../assets/images/quality.svg'
-import webTesting from '../../assets/images/webTesting.svg'
-import usability from '../../assets/images/usability.svg'
-import automation from '../../assets/images/automation.svg'
-import performance from '../../assets/images/performance.svg'
 import { useInView } from "react-intersection-observer";
 import AnimateOnScroll from "@/components/general/AnimateOnScroll.jsx";
+import ServicesData from "@/constants/Testing/ServicesData";
 
 const SoftwareTesting = () => {
     const [ref, inView] = useInView({
@@ -17,36 +11,13 @@ const SoftwareTesting = () => {
         <section ref={ref} className="container mt-[72px] mb-[120px]">
             <AnimateOnScroll inView={inView} direction={"leftToRight"}>
                 <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-12">
-                    <Services
-                        image={mobApp}
-                        title="Mobile App Testing"
-                        paragraph="Mobile application testing is a process by which application software developed for handheld mobile devices is tested for its functionality, usability, and consistency."
-                    />
-                    <Services
-                        image={quality}
-                        title="Functional Testing"
-                        paragraph="Functional testing is a way of checking software to ensure that it has all the required functionality that's specified within its functional requirements."
-                    />
-                    <Services
-                        image={webTesting}
-                        title="Web Testing"
-                        paragraph="Web application testing is a technique exclusively adopted to test the applications that are hosted on web in which the application interfaces and other functionalities are tested."
-                    />
-                    <Services
-                        image={usability}
-                        title="Usability Testing"
-                        paragraph="Usability testing is a way to see how easy to use something is by testing it with real users. Users are asked to complete tasks, typically while they are being observed by a researcher, to see where they encounter problems and experience confusion."
-                    />
-                    <Services
-                        image={automation}
-                        title="Automation Testing"
-                        paragraph="Automated software testing can increase the depth and scope of tests to help improve software quality, saving cost and time."
-                    />
-                    <Services
-                        image={performance}
-                        title="Performance Testing"
-                        paragraph="In software engineering, performance testing is in general, a testing practice performed to determine how a system performs in terms of responsiveness and stability under a particular workload."
-                    />
+                    {ServicesData.map((service, index) => (
+                        <div key={index} className="mt-14">
+                            <img src={service.image} className="w-10 h-10 rounded-2xl" alt={service.title} />
+                            <h2 className="text-[#121212] font-bold text-4xl mt-4">{service.title}</h2>
+                            <p className="w-[373px] text-[#5D5D5D] font-medium text-2xl mt-4">{service.paragraph}</p>
+                        </div>
+                    ))}
                 </div>
             </AnimateOnScroll>
         </section>
---
+++ src/constants/Testing/ServicesData.jsx
@@ -0,0 +1,41 @@
+import mobApp from '../../assets/images/mobApp.svg'
+import quality from '../../assets/images/quality.svg'
+import webTesting from '../../assets/images/webTesting.svg'
+import usability from '../../assets/images/usability.svg'
+import automation from '../../assets/images/automation.svg'
+import performance from '../../assets/images/performance.svg'
+const ServicesData = [
+    {
+        image: mobApp,
+        title: "Mobile App Testing",
+        paragraph: "Mobile application testing is a process by which application software developed for handheld mobile devices is tested for its functionality, usability, and consistency."
+    },
+    {
+        image: quality,
+        title: "Functional Testing",
+        paragraph: "Functional testing is a way of checking software to ensure that it has all the required functionality that's specified within its functional requirements."
+    },
+    {
+        image: webTesting,
+        title: "Web Testing",
+        paragraph: "Web application testing is a technique exclusively adopted to test the applications that are hosted on web in which the application interfaces and other functionalities are tested."
+    },
+    {
+        image: usability,
+        title: "Usability Testing",
+        paragraph: "Usability testing is a way to see how easy to use something is by testing it with real users. Users are asked to complete tasks, typically while they are being observed by a researcher, to see where they encounter problems and experience confusion."
+    },
+    {
+        image: automation,
+        title: "Automation Testing",
+        paragraph: "Automated software testing can increase the depth and scope of tests to help improve software quality, saving cost and time."
+    },
+    {
+        image: performance,
+        title: "Performance Testing",
+        paragraph: "In software engineering, performance testing is in general, a testing practice performed to determine how a system performs in terms of responsiveness and stability under a particular workload."
+    },
+
+]
+
+export default ServicesData
