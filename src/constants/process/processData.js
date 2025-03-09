85e311a271bdeebc7a416f7016c45ba8ae66a0c3
Ahmed <ahmedabozaid.dev@gmail.com>
2025-03-09 08:26:43 +0200

    adds process data

--- src/constants/process/processData.js
+++ src/constants/process/processData.js
@@ -3,27 +3,54 @@ import DesignIcon from "@/components/icons/process/DesignIcon.jsx";
 import DesignHoveredIcon from "@/components/icons/process/DesignHoveredIcon.jsx";
 import DevelopmentIcon from "@/components/icons/process/DevelopmentIcon.jsx";
 import DevelopmentHoveredIcon from "@/components/icons/process/DevelopmentHoveredIcon.jsx";
+import TestingIcon from "@/components/icons/process/TestingIcon.jsx";
+import TestingHoveredIcon from "@/components/icons/process/TestingHoveredIcon.jsx";
+import LaunchIcon from "@/components/icons/process/LaunchIcon.jsx";
+import LaunchHoveredIcon from "@/components/icons/process/LaunchHoveredIcon.jsx";
+import SalesIcon from "@/components/icons/process/SalesIcon.jsx";
+import SalesHoveredIcon from "@/components/icons/process/SalesHoveredIcon.jsx";
 
 export const processData = [
   {
     id: 1,
-    name: "Analysis",
+    name: "01. Analysis And Planning",
     body: "body",
     icon: <AnalysisIcon />,
     hoveredIcon: <AnalysisIcon />,
   },
   {
-    id: 1,
-    name: "Design",
+    id: 2,
+    name: "02. Design",
     body: "body",
     icon: <DesignIcon />,
     hoveredIcon: <DesignHoveredIcon />,
   },
   {
-    id: 1,
-    name: "Development",
+    id: 3,
+    name: "03. Software Development",
     body: "body",
     icon: <DevelopmentIcon />,
     hoveredIcon: <DevelopmentHoveredIcon />,
+  },
+  {
+    id: 4,
+    name: "04. Testing",
+    body: "body",
+    icon: <TestingIcon />,
+    hoveredIcon: <TestingHoveredIcon />,
+  },
+  {
+    id: 5,
+    name: "05. Launch",
+    body: "body",
+    icon: <LaunchIcon />,
+    hoveredIcon: <LaunchHoveredIcon />,
+  },
+  {
+    id: 6,
+    name: "06. After-Sales Service",
+    body: "body",
+    icon: <SalesIcon />,
+    hoveredIcon: <SalesHoveredIcon />,
   },
 ];
