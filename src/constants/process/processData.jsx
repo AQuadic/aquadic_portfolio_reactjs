288944fda239ef667668654c5cac3da0ebb1a843
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-03-09 11:29:51 +0200

    Company process data

--- src/constants/process/processData.jsx
+++ src/constants/process/processData.jsx
@@ -14,42 +14,53 @@ export const processData = [
   {
     id: 1,
     name: "01. Analysis And Planning",
-    body: "body",
+    body: (
+      <div className="flex flex-col gap-4 text-[#121212] text-base">
+        <p className="font-medium">Answering the big questions:.</p>
+        <ul className="flex flex-col gap-3">
+          <li>Who is supposed to use this software?</li>
+          <li>Who is supposed to use this software?</li>
+          <li>Who is supposed to use this software?</li>
+          <li>Who is supposed to use this software?</li>
+        </ul>
+      </div>
+    ),
     icon: <AnalysisIcon />,
     hoveredIcon: <AnalysisIcon />,
-  },
+  }
+  ,
   {
     id: 2,
     name: "02. Design",
-    body: "body",
+    body: "What this software will look like and how it will function In this phase a draft design is created to show us if the software requirements will work before we start the code. Test strategies are developed in this phase by testers by mentioning things to test and ways to check it. ",
     icon: <DesignIcon />,
     hoveredIcon: <DesignHoveredIcon />,
   },
   {
     id: 3,
     name: "03. Software Development",
-    body: "body",
+    body: "Where actual coding begins In this phase our experienced developers start breathing life into the design through code. This is the longest phase, but you’ll be updated all through it so you always know where we are.",
     icon: <DevelopmentIcon />,
     hoveredIcon: <DevelopmentHoveredIcon />,
   },
   {
     id: 4,
     name: "04. Testing",
-    body: "body",
+    body: "Testing, tracking and fixing bugs We need to know before hand what isn’t working, and fix it, because it increases end-customer satisfaction from the very beginning.",
     icon: <TestingIcon />,
     hoveredIcon: <TestingHoveredIcon />,
   },
   {
     id: 5,
     name: "05. Launch",
-    body: "body",
+    body: "Pushing the code into production After the development of the application is completed and it has been thoroughly tested, it can be finally released to the market and made available for the users. This phase includes functional testing, way of acceptance criteria validation. We always monitor the product closely and ensure it works as intended after launch.",
     icon: <LaunchIcon />,
     hoveredIcon: <LaunchHoveredIcon />,
   },
   {
     id: 6,
     name: "06. After-Sales Service",
-    body: "body",
+    body: "We’re not done yet! After the launch is over, we’ll continue support you by improving the software to make sure it’s bug-free and operational. We’ll listen to what your users have to say, and maybe make a few changes, and this phase lasts as the product lasts.",
     icon: <SalesIcon />,
     hoveredIcon: <SalesHoveredIcon />,
   },
